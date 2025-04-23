7c71d6dc4e5ad3ea8eaffd9d3fc5ca9f93602009
MOCDATA-PTY <mocptydata@gmail.com>
2025-04-23 01:34:07 -0600

    Changed Allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-su51u4*nn&5rg0f0jpl3ojbnoydml&)8$@wu1l+mjs^+24sx&7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
